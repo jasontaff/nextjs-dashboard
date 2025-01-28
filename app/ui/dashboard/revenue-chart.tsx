3a8218bf3055a302a0d70b35b73a2d1bcf812a64
jtaff <jtaff@2u.com>
2025-01-28 16:21:17 -0500

    fix error

--- app/ui/dashboard/revenue-chart.tsx
+++ app/ui/dashboard/revenue-chart.tsx
@@ -9,11 +9,7 @@ import { fetchRevenue } from '@/app/lib/data'
 // https://www.chartjs.org/
 // https://airbnb.io/visx/
 
-export default async function RevenueChart({
-
-}: {
-  revenue: Revenue[];
-}) {
+export default async function RevenueChart() {
 
   const revenue = await fetchRevenue();
   const chartHeight = 350;
