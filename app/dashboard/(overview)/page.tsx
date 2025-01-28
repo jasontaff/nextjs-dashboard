b6ca88b0fe1f4a272408d50fecc94285cba734e9
jtaff <jtaff@2u.com>
2025-01-28 17:51:02 -0500

    start of search and pagination

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -3,13 +3,14 @@ import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
 import { Suspense } from 'react';
-import { fetchRevenue, fetchLatestInvoices, fetchCardData} from '../../lib/data';
-import { RevenueChartSkeleton } from '@/app/ui/skeletons';
+import { fetchRevenue, fetchLatestInvoices} from '../../lib/data';
+import { RevenueChartSkeleton, LatestInvoicesSkeleton, CardsSkeleton} from '@/app/ui/skeletons';
+import CardWrapper from '@/app/ui/dashboard/cards';
  
 export default async function Page() {
 
     const latestInvoices  = await fetchLatestInvoices();
-    const {numberOfCustomers, numberOfInvoices, totalPaidInvoices, totalPendingInvoices} = await fetchCardData()
+
 
   return (
     <main>
@@ -17,21 +18,20 @@ export default async function Page() {
         Dashboard
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-         <Card title="Collected" value={totalPaidInvoices} type="collected" /> 
-         <Card title="Pending" value={totalPendingInvoices} type="pending" /> 
-         <Card title="Total Invoices" value={numberOfInvoices} type="invoices" /> 
-         <Card
-          title="Total Customers"
-          value={numberOfCustomers}
-          type="customers"
-        /> 
+       <Suspense fallback={<CardsSkeleton/>}>
+        <CardWrapper />
+       </Suspense>
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
         <Suspense fallback={<RevenueChartSkeleton />}>
           <RevenueChart />
         </Suspense>
-        {/* <RevenueChart revenue={revenue}  /> */}
-        <LatestInvoices latestInvoices={latestInvoices} /> 
+
+        <Suspense fallback={<LatestInvoicesSkeleton/>} >
+          <LatestInvoices />  
+        </Suspense>
+
+
       </div>
     </main>
   );
